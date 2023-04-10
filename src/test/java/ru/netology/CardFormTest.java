98455833318f53619ef719625b3ee3a4d7f8927e
Yuriy <yura.qa53@gmail.com>
2023-04-10 16:14:48 +0300

    first commit

--- src/test/java/ru/netology/CardFormTest.java
+++ src/test/java/ru/netology/CardFormTest.java
@@ -1,36 +1,23 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CardFormTest {
-    @Test
-    void shouldSendForm() {
-        Configuration.headless = true;
-        Configuration.holdBrowserOpen = true;
-//        LocalDate date = LocalDate.now();
-        String date = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    @BeforeEach
+    void setupTest(){
         open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Южно-Сахалинск");
-        $("[data-test-id='date'] input").doubleClick();
-        $("[data-test-id='date'] input").sendKeys(Keys.DELETE);
-//        $("[data-test-id='date'] input").setValue(date.plusDays(5).format(DateTimeFormatter.ofPattern("dd MM yyyy")));
-        $("[data-test-id='date'] input").setValue(date);
-        $("[data-test-id='name'] input").setValue("Жан-Поль Бельмондо");
-        $("[data-test-id='phone'] input").setValue("+12345678910");
+    }
+    @Test
+    void shouldSuccessfulSendValidForm() {
+        $("[data-test-id=name] input").setValue("Иванов Иван");
+        $("[data-test-id=phone] input").setValue("+79287215679");
         $("[data-test-id=agreement]").click();
-        $$("button").find(Condition.exactText("Забронировать")).click();
-        $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
-//        $(".notification__content").shouldHave(Condition.ownText(date.plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))));
-        $(".notification__content").shouldHave(Condition.ownText(date));
+        $(".button").click();
+        $(".Success_successBlock__2L3Cw").shouldHave(text(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
-
